42cdea5cb8626ff6bd63b2f97fdd15e3b3668a33
Alejandro Ruiz Sánchez <alejandroruizsanchez2001@gmail.com>
2021-05-13 17:47:31 +0200

    ArgoUML

---
+++ src/Empleado.java
@@ -0,0 +1,52 @@
+import java.util.Vector;
+
+public class Empleado {
+
+  public Integer codigo;
+
+  public String nombre;
+
+  public Integer newAttr;
+
+  public Integer salario;
+
+    public Departamento lugardetrabajo;
+    public Empleado empleadojefe;
+    /**
+   * 
+   * @element-type Empleado
+   */
+  public Vector  empleado;
+
+  public Integer getcodigo() {
+  return null;
+  }
+
+  public String getnombre() {
+  return null;
+  }
+
+  public String getoficio() {
+  return null;
+  }
+
+  public Integer getsalario() {
+  return null;
+  }
+
+  public void setcodigo(Integer cod) {
+  }
+
+  public void setnombre(String nom) {
+  }
+
+  public void setoficio(String ofi) {
+  }
+
+  public void setsalario(Integer sal) {
+  }
+
+  public Empleado() {
+  }
+
+}
